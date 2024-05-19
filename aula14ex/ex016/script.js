11948c72f38c18b8dd6e830922d415f836c1c1ae
Emeyas Franco <emeyasdev@gmail.com>
2024-05-19 16:48:50 -0300

    exercícios 16 e 17

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,11 +1,27 @@
 function contar() {
-    let inicio = document.querySelector('input#inicio')
-    let fim = document.querySelector(`input#fim`)
-    let passo = document.querySelector(`input#passo`)
+    let inicio = document.querySelector('input#txti')
+    let fim = document.querySelector(`input#txtf`)
+    let passo = document.querySelector(`input#txtp`)
     let res = document.querySelector('div#res')
-  
-    while (inicio.value <= fim.value) {
-        res.innerHTML = `Número ${inicio.value}`
-        inicio.value++
+    
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = `Impossível contar`
+    } else {
+        res.innerHTML = `<p>Contando</p>`
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f){
+            //contagem crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            //contagem regressiva 
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
     }
 }
